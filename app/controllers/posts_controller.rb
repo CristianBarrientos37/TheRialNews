d59ccecace9a544d6a8f16d06f15c98aa524c373
CristianBarrientos37 <cristiantj_cbm@hotmail.com>
2024-10-18 12:17:26 -0300

    tercer commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
   before_action only: [:new, :create] do
-    authorize_request(["author", "admin"])
+    authorize_request(["normal_user","author", "admin"])
   end
 
   before_action only: [:edit, :update, :destroy] do
